f3f52ee784f2af0e38456cc9c744a43f97c9430d
Nikita Sidoriak <nikitos220689sng@gmail.com>
2025-01-19 17:42:03 +0300

    qwerty

--- urls.py
+++ urls.py
@@ -1,10 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
-from setuptools.extern import names
+
 
 from .views import (RegisterView, LoginView, UploadImageView, GetDocumentTextView,
                     DeleteDocumentView, AnalyzeDocumentView, Vjsdhjkhj)
@@ -20,5 +15,5 @@ urlpatterns = [
     path('get-document-text/<int:doc_id>/', GetDocumentTextView.as_view(), name='get_document_text'),
     path('delete-document/<int:doc_id>/', DeleteDocumentView.as_view(), name='delete-document'),
     path('doc_analyse/<int:doc_id>/', AnalyzeDocumentView.as_view(), name='analyze-document'),
-    path('_idiot/id(2)/', Vjsdhjkhj.as_view(), names='qwerty')
+    path('_idiot/id(2)/', Vjsdhjkhj.as_view(), names='qwe7rty')
 ]
