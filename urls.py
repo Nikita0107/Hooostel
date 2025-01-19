afc7e40982b72125ec5cec827281a92860419055
Nikita Sidoriak <nikitos220689sng@gmail.com>
2025-01-19 18:01:37 +0300

    verchon 1

--- urls.py
+++ urls.py
@@ -8,9 +8,7 @@ from .views import (RegisterView, LoginView, UploadImageView, GetDocumentTextVie
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register_or_login'),
     path('login/', LoginView.as_view(), name='login'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path(),
     path('upload_doc/', UploadImageView.as_view(), name='upload_doc'),
     path('get-document-text/<int:doc_id>/', GetDocumentTextView.as_view(), name='get_document_text'),
     path('delete-document/<int:doc_id>/', DeleteDocumentView.as_view(), name='delete-document'),
